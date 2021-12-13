d2c786a760210b4f9d95f4785a5557fb71dc2aba
[dylanjwu] <dylanjwu@gmail.com>
2021-12-13 09:03:29 -0500

    refactor graph creator to use multigraph

--- src/random_metric_graph.py
+++ src/random_metric_graph.py
@@ -1,16 +1,19 @@
 
 import random as r
 import numpy as np
+import networkx as nx
+
 """
 Helper function to check that random edge obeys the triangle inequality/is metric space
 """
-def obeysMetric(g, i, j):
+def obeysMetric(g, i, j, candidate_weight):
     for k in range(len(g)):
         # triangle inequality: edge weight w(i->j) should be less than or equal to w(i->k)+w(k->j)
-        if g[i][k] == 0 or g[j][k] == 0: 
-            continue
-        if g[i][k]+g[k][j] < g[i][j]:
-            return False
+        if g.has_edge(i, k) and g.has_edge(k, j): 
+            ik_weight = g.get_edge_data(i, k)[0]['weight']
+            kj_weight = g.get_edge_data(k, j)[0]['weight']
+            if ik_weight + kj_weight < candidate_weight:
+                return False
     return True
 
 """
@@ -20,15 +23,23 @@ w_rnge: default edge weight range is 1 to 10
 return: adjacency matrix representing complete metric graph
 """
 def randCompleteMetricGraph(n, w_rnge = (1,10)):
-    g = [[0 for _ in range(n)] for _ in range(n)] # initialize adjacency matrix
+    
+    g = nx.MultiGraph()
+
     for i in range(n):
+
+        g.add_node(i)
+
         for j in range(n):
-            if g[j][i] != 0: # matrix is symmetric b/c graph's undirected
-                g[i][j] = g[j][i]
-            elif i != j: #if vertex is not itself, create random edge
-                g[i][j] = r.randint(w_rnge[0], w_rnge[1])
-                while not obeysMetric(g, i, j):
-                    g[i][j] = r.randint(w_rnge[0], w_rnge[1])
+
+            if i != j and not g.has_edge(i, j): # no self loops, no double edges
+                candidate_weight = r.randint(w_rnge[0], w_rnge[1])
+
+                while not obeysMetric(g, i, j, candidate_weight):
+                    candidate_weight = r.randint(w_rnge[0], w_rnge[1])
+
+                g.add_edge(i, j, weight=candidate_weight)
     return g
 
-g = randCompleteMetricGraph(10)
+g = randCompleteMetricGraph(5)
+print(g.adj)

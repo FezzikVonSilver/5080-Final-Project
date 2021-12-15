82eeeb6b2ee7653199fc3d4007c90d4fc7c43667
Paul-LeBorgne <80861214+Paul-LeBorgne@users.noreply.github.com>
2021-12-14 22:42:33 -0500

    additional testing, graph creation option

--- src/viz_testing.py
+++ src/viz_testing.py
@@ -41,7 +41,7 @@ def randCompleteMetricGraph(n, w_rnge = (1,10)):
     return g
 
 
-g = randCompleteMetricGraph(10)
+g = randCompleteMetricGraph(5)
 
 # convert the Graph adjacency matrix array to a numpy matrix
 g2 = np.matrix(g)
@@ -50,70 +50,60 @@ g2 = np.matrix(g)
 g3 = nx.Graph(from_numpy_array(g2))
 
 
-# set position using spring layout (this should result in the nodes displaying at the same position each time the same graph object is drawn if used in call)
+
+"""
+Function to create graph from a simple 2d-array
+Takes the 
+
+n: number of vertices in the complete graph
+G: adjacency matrix representing complete metric graph 
+
+return: a networkx graph object
+"""
+def create_nwx_Graph(n, G):
+        g4 = nx.Graph()
+
+        for i in range (n):
+            g4.add_node(i)
+
+    #Adds edges along with their weights to the graph 
+        for i in range(n) :
+         for j in range(n)[i:] :
+          if G[i][j] != 0 :
+               g4.add_edge(i, j, weight = G[i][j]) 
+               print(G[i][j])
+        return g4
+
+
+
+
+g3 = create_nwx_Graph(5,g)
+print(g3)
+
+for edge in g3.edges:
+    print(edge)
+
+
+print(g)
+
+
+
+# set position using spring layout (this will result in the nodes displaying at the same position each time the same graph object is drawn if used in call)
 pos = nx.spring_layout(g3, seed=123)
 
-
-# # draw the nodes only for the graph
-# nx.draw_networkx_nodes(g3, pos, nodelist=set(g3.nodes))
-# # display the graph
-# plt.show()
-
-# # draw the edges only for the graph 
-# nx.draw_networkx_edges(g3, pos, edgelist=set(g3.edges))
-# # display the graph
-# plt.show()
-
-# # draw the full graph 
-# nx.draw_networkx(g3, pos)
-# # display the graph
-# plt.show()
+# get the edge weights as labels
+labels = nx.get_edge_attributes(g3,'weight')
+# draw the graph
+nx.draw_networkx(g3, pos)
+# add the edge weights to the graph
+nx.draw_networkx_edge_labels(g3,pos,edge_labels=labels)
+plt.show()
 
 
-# # Get a shortest path (this will be only 1 edge long as the graph is complete) 
-# path = nx.shortest_path(g3,source=1,target=7)
-# path_edges = list(zip(path,path[1:]))
 
-# # Draw nodes and edges not in the shortest path
-# nx.draw_networkx_nodes(g3, pos, nodelist=set(g3.nodes)-set(path))
-# # draw the edges not in the shortest path
-# nx.draw_networkx_edges(g3, pos, edgelist=set(g3.edges)-set(path_edges), connectionstyle='arc3, rad = 0.3')
-
-# # Draw nodes and edges included in path, both as red
-# nx.draw_networkx_nodes(g3, pos, nodelist=path, node_color='r')
-# nx.draw_networkx_edges(g3,pos,edgelist=path_edges,edge_color='r', connectionstyle='arc3, rad = 0.3')
-
-# # display the graph
-# plt.show()
-
-
-# # create a TSP object to use
-# tsp = nx.approximation.traveling_salesman_problem
-
-# # run the TSP function using Christofides and save the path
-# path = tsp(g3, cycle=False, method=christofides)
-# # print(path)
-
-# path_edges = list(zip(path,path[1:]))
-
-# # draw the edges not in the TSP path
-# # nx.draw_networkx_edges(g3, pos, edgelist=set(g3.edges)-set(path_edges), connectionstyle='arc3, rad = 0.3')
-
-# # Draw nodes and edges included in path, nodes blue and edges red
-# nx.draw_networkx_nodes(g3, pos, nodelist=path, node_color='b')
-# nx.draw_networkx_edges(g3,pos,edgelist=path_edges,edge_color='r', connectionstyle='arc3, rad = 0.3')
-
-# # display the graph
-# plt.show()
-
-# # plt.get
-# input("Press Enter to continue...")
-# plt.close()
-
+# create fig object for animation
 fig = plt.figure()
 
-
-nx.draw(g3, with_labels=True)
 # Function to animate a series of steps which can be displayed
 def animate(frame):
     fig.clear()
@@ -123,47 +113,36 @@ def animate(frame):
         # draw the nodes only for the graph
         nx.draw_networkx_nodes(g3, pos, nodelist=set(g3.nodes))
         plt.show()
-        # pause until user presses the Enter button
-        # input("Press Enter to continue...")
-        # time.sleep(5)
 
     if frame == 2:
         # draw the edges only for the graph 
         nx.draw_networkx_edges(g3, pos, edgelist=set(g3.edges))
         plt.show()
-        # pause until user presses the Enter button
-        # input("Press Enter to continue...")
-        # time.sleep(5)
 
     if frame ==3:
         # draw the full graph 
         nx.draw_networkx(g3, pos)
         plt.show()
-        # pause until user presses the Enter button
-        # input("Press Enter to continue...")
-        # time.sleep(5)
 
     if frame ==4:
         # Get a shortest path (this will be only 1 edge long as the graph is complete) 
-        path = nx.shortest_path(g3,source=1,target=7)
+        path = nx.shortest_path(g3,source=1,target=4)
         path_edges = list(zip(path,path[1:]))
         # Draw nodes and edges not in the shortest path
         nx.draw_networkx_nodes(g3, pos, nodelist=set(g3.nodes)-set(path))
         nx.draw_networkx_edges(g3, pos, edgelist=set(g3.edges)-set(path_edges), connectionstyle='arc3, rad = 0.3')
         # Draw nodes and edges included in path, both as red
-        nx.draw_networkx_nodes(g3, pos, nodelist=path, node_color='r')
-        nx.draw_networkx_edges(g3,pos,edgelist=path_edges,edge_color='r', connectionstyle='arc3, rad = 0.3')
+        # nx.draw_networkx_nodes(g3, pos, nodelist=path, node_color='r')
+        nx.draw_networkx_nodes(g3, pos, nodelist=path)
+        nx.draw_networkx_edges(g3, pos, edgelist=path_edges,edge_color='r', connectionstyle='arc3, rad = 0.3')
         plt.show()
-        # pause until user presses the Enter button
-        # input("Press Enter to continue...")
-        # time.sleep(5)
 
     if frame >=5:
         # create a TSP object to use
         tsp = nx.approximation.traveling_salesman_problem
         # run the TSP function using Christofides and save the path
-        path = tsp(g3, cycle=False, method=christofides)
-        # print(path)
+        path = tsp(g3, cycle=True, method=christofides)
+        print(path)
         # get the edges in a list
         path_edges = list(zip(path,path[1:]))
         # draw the edges not in the TSP path
@@ -172,27 +151,11 @@ def animate(frame):
         nx.draw_networkx_nodes(g3, pos, nodelist=path, node_color='b')
         nx.draw_networkx_edges(g3,pos,edgelist=path_edges,edge_color='r', connectionstyle='arc3, rad = 0.3')
         plt.show()
-        # pause until user presses the Enter button
-        # input("Press Enter to continue...")
-        # time.sleep(5)
 
-    
     return
 
 # create the animation object
-ani = animation.FuncAnimation(fig, animate, frames=6, interval=1000, repeat=False)
-plt.show()
-# def animate2(frame):
-#     fig.clear()
+# ani = animation.FuncAnimation(fig, animate, frames=6, interval=1000, repeat=False)
 
-#     # for i in range (6):
-#     if frame <= 1:
-#         # draw the nodes only for the graph
-#         nx.draw_networkx_nodes(g3, pos, nodelist=set(g3.nodes))
-#         plt.show()
-#         # pause until user presses the Enter button
-#         # input("Press Enter to continue...")
-#         # time.sleep(5)
-
-# ani2 = animation.FuncAnimation(fig, animate2, frames=2, interval=3000, repeat=False)
+# display the animation on screen in a popup window
 # plt.show()

a8caebe16542d1d3bb55c80cf6f078c28bec3896
Paul-LeBorgne <80861214+Paul-LeBorgne@users.noreply.github.com>
2021-12-16 00:32:51 -0500

    updated for revised branch files

--- src/tsp_animation.py
+++ src/tsp_animation.py
@@ -9,6 +9,8 @@ import random_metric_graph as rmg
 from min_span_tree import mst
 import combine as com
 from create_tour import create_tour
+import christofides as ch
+
 
 
 """
@@ -35,11 +37,14 @@ def translate_TSP_Tour(n, TSP):
 def christo_animation(n):
 
     # create the starting random graph
-    g = rmg.randCompleteMetricGraph(n)
+    g = rmg.randCompleteMetricGraph(6)
+    # g = com.span_root_example()
 
     # get the weights of the edges to label in graph
     edge_labels = dict([((n1, n2), d['weight'])
                     for n1, n2, d in g.edges(data=True)])
+
+    # labels = nx.get_edge_attributes(g, 'weight')
 
     # set position using spring layout (this will result in the nodes displaying at the same position each time the same graph object is drawn if used in call)
     # pos = nx.spring_layout(g, seed=1)
@@ -57,6 +62,11 @@ def christo_animation(n):
     M2 = com.combine(T, g)
     # get tour
     tsp_tour = create_tour(M2, g)
+    tour_edges = zip(list(tsp_tour))
+    # get tour cost, wrong?
+    tour_cost = str(round(ch.get_cost(g, tsp_tour), 2))
+    
+
     # translate tour to draw easier
     g_tsp = translate_TSP_Tour(n+1, tsp_tour)
 
@@ -71,9 +81,10 @@ def christo_animation(n):
         # for i in range (6):
         if frame == 0:
             # draw the full graph 
-            nx.draw_networkx(g, pos)
+            nx.draw_networkx(g, pos, with_labels = True)
+            # nx.draw(g, with_labels = True)
             # draw edge weight labels for troubleshooting
-            # nx.draw_networkx_edge_labels(g, pos, edge_labels=edge_labels)
+            nx.draw_networkx_edge_labels(g, pos, edge_labels=edge_labels)
             plt.title("Starting Graph", loc='center')
             plt.show()
             plt.savefig("figure1.png")
@@ -119,6 +130,7 @@ def christo_animation(n):
         # draw combined min-weight matching and MST
         if frame == 5:
             nx.draw_networkx(M2, pos)
+            # nx.draw_networkx_edges(g, pos, edgelist=tsp_tour, width=6)
             plt.title("Minimum Spanning Tree & Min-Weight Matching Combined", loc='center')
             plt.show()
             plt.savefig("figure6.png")
@@ -129,7 +141,10 @@ def christo_animation(n):
         # draw our TSP tour
         if frame >= 6:
             nx.draw_networkx(g_tsp, pos, edge_color='r')
-            plt.title("Christofides TSP Tour", loc='center')
+            # nx.draw_networkx_nodes(g, pos)
+            # nx.draw_networkx_edges(g, pos, edgelist=set(tour_edges), edge_color='r')
+            # add label with tour cost rounded to 3 decimals
+            plt.title("Christofides TSP Tour - cost: " + tour_cost, loc='center')
             plt.show()
             plt.savefig("figure7.png")
             # print(tsp_tour)
@@ -139,11 +154,11 @@ def christo_animation(n):
         return
 
     # create the animation object, interval is the time each frame (image) is displayed in milliseconds
-    ani = animation.FuncAnimation(fig, animate, frames=7, interval=2000, repeat=False)
+    ani = animation.FuncAnimation(fig, animate, frames=7, interval=1000, repeat=False)
 
     # display the animation on screen in a popup window
     plt.show()
 
 #main function
 if __name__ == "__main__":
-    christo_animation(8)
+    christo_animation(6)
